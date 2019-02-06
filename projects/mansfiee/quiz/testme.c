0130ca3118f69916da1bc7d66c92d8c55f843ec2
elisabethmansfield <elisjmansfield@gmail.com>
2019-02-05 22:23:36 -0500

    Random Testing Quiz

---
+++ projects/mansfiee/quiz/testme.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+    // return a random char between ASCII 32 to 126
+    return (char)(rand() % 95 + 32);
+}
+
+char *inputString()
+{
+    // declare the string
+    static char s[6];
+    memset(&s, 0, 5);
+    // limit range of values to chars in 'reset'
+    char range[] = {'e','r','s','t'};
+    int index;
+    int i;
+    // assign char values to the string
+    for (i=0; i<5; i++){
+        index = (rand() % 4);
+        s[i] = range[index];
+    }
+    s[5] = '\0';
+    
+    return s;
+}
+
+void testme()
+{
+    int tcCount = 0;
+    char *s;
+    char c;
+    int state = 0;
+    while (1)
+    {
+        tcCount++;
+        c = inputChar();
+        s = inputString();
+        printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+        
+        if (c == '[' && state == 0) state = 1;
+        if (c == '(' && state == 1) state = 2;
+        if (c == '{' && state == 2) state = 3;
+        if (c == ' '&& state == 3) state = 4;
+        if (c == 'a' && state == 4) state = 5;
+        if (c == 'x' && state == 5) state = 6;
+        if (c == '}' && state == 6) state = 7;
+        if (c == ')' && state == 7) state = 8;
+        if (c == ']' && state == 8) state = 9;
+        if (s[0] == 'r' && s[1] == 'e'
+            && s[2] == 's' && s[3] == 'e'
+            && s[4] == 't' && s[5] == '\0'
+            && state == 9)
+        {
+            printf("Error 200: \'reset\' encountered.\n");
+            exit(200);
+        }
+    }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
+
